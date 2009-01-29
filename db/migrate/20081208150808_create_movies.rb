0eb94330f1b4a19b0b565de52dbb1c9fd04cc9c4
Vitaly Kushner <vitaly-at-astrails-dot-com>
2009-01-29 15:07:03 +0200

    use innodb and utf8

--- db/migrate/20081208150808_create_movies.rb
+++ db/migrate/20081208150808_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration
   def self.up
-    create_table :movies do |t|
+    create_table :movies, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
       t.string :title
       t.text :description
       t.datetime :date
